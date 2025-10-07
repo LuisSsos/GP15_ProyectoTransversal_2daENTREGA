62be983e3ae901fc5a9e60e4e7321fa48a0b65ee
Luca Rodrigaño <Lucarodrigano@gmail.com>
2025-10-07 20:24:55 -0300

    Cree la clase Materia
    Atributos, constructores, getters, setters y toString

---
+++ GP15_2daENTREGA/src/Modelo/Materia.java
@@ -0,0 +1,62 @@
+package Modelo;
+
+/** 
+    @author Grupo 15
+    Luis Ezequiel Sosa
+    Lucas Saidman
+    Luca Rodrigaño
+    Ignacio Rodriguez
+**/
+
+public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int cuatrimestre;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre, int cuatrimestre, boolean estado) {
+        this.nombre = nombre;
+        this.cuatrimestre = cuatrimestre;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCuatrimestre() {
+        return cuatrimestre;
+    }
+
+    public void setCuatrimestre(int cuatrimestre) {
+        this.cuatrimestre = cuatrimestre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", cuatrimestre=" + cuatrimestre + ", estado=" + estado + '}';
+    }     
+}
